ef08722627b47a05a6ed13d40a2541d6351c5942
Bojan Serafimov <bojan@pop-os.localdomain>
2021-07-07 21:23:01 -0400

    Fix tests

--- tests/test_dry_run.py
+++ tests/test_dry_run.py
@@ -1,37 +1,36 @@
 from dry_run import trouble
 
 
-write_to_s3 = object()
+send_email = object()
 
 
-@trouble.could_cause(write_to_s3)
-def job_1():
+@trouble.could_cause(send_email)
+def send_email_to_client(destination, content):
     """Function that maybe writes to s3."""
     # Just pretend it does
 
 
 @trouble.could_only_cause(trouble.nothing)
-def command_1():
-    job_1()
+def rebuild_cache():
+    # ...
+    # This shouldn't be allowed
+    send_email_to_client("alice@wonder.land", "Hello")
 
 
-@trouble.could_only_cause(trouble.only(write_to_s3))
-def command_2():
-    job_1()
+@trouble.could_only_cause(trouble.only(send_email))
+def process_request(request):
+    # ...
+    send_email_to_client("alice@wonder.land", "Hello")
 
 
 def test_1():
-    # Running job_1 directly shouldn't raise
-    job_1()
-
-    # command_1 possibly causes write_to_s3 trouble
-    # without declaring that. It should raise.
+    # rebuild_cache should be prevented from sending email
     try:
-        command_1()
+        rebuild_cache()
         assert False
     except trouble.PossibleTrouble as exc:
-        assert exc.trouble_token == write_to_s3
+        assert exc.trouble_token == send_email
 
-    # command_2 declares that it's safe to write_to_s3
-    # so it should't raise trouble.PossibleTrouble.
-    command_2()
+    # We might need to send an email while processing a
+    # request. Verify that it's allowed.
+    process_request({})
